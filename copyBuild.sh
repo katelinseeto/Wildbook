52e20c15e59ab172637b24911a805efa9629ddc2
Mark <mark.aaron.fisher@gmail.com>
2020-05-16 20:07:49 -0700

    minor changes to hopefully improve copyBuild.sh and copyBuildDirOnly.sh. Incomplete

--- copyBuild.sh
+++ copyBuild.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-echo \ * copying resources...
-sudo service tomcat8 stop
-sudo cp target/wildbook-7.0.0-EXPERIMENTAL.war /var/lib/tomcat8/webapps/wildbook.war
-sudo service tomcat8 start
-echo \ * copyBuild complete.
+echo "...copying resources..." && sudo service tomcat8 stop && sudo cp target/wildbook-7.0.0-EXPERIMENTAL.war /var/lib/tomcat8/webapps/wildbook.war && sudo chown tomcat8 /var/lib/tomcat8/webapps/wildbook.war && sudo chgrp tomcat8 /var/lib/tomcat8/webapps/wildbook.war && sudo service tomcat8 start & echo "...copyBuild completed successfully"
